d97203c4fcd8406e6caaee20f07f7370e22b10e6
Alper Yurtdaş <73263073+Alperyrtds@users.noreply.github.com>
2021-07-15 19:49:23 +0300

    Last State

--- MVCLMS/Controllers/LoginController.cs
+++ MVCLMS/Controllers/LoginController.cs
@@ -3,15 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCLMS.Models.Entity;
+using System.Web.Security;
 
 namespace MVCLMS.Controllers
 {
     public class LoginController : Controller
     {
-        // GET: Login
+        DBLibraryEntities database = new DBLibraryEntities();
         public ActionResult SignIn()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult SignIn(Users users)
+        {
+            var data = database.Users.FirstOrDefault(x => x.Mail == users.Mail && x.Password == users.Password);
+            if (data != null)
+            {
+                FormsAuthentication.SetAuthCookie(data.Mail, false);
+                Session["Mail"] = data.Mail.ToString();
+                //TempData["Name"] = data.Name.ToString();
+                //TempData["Id"] = data.Id.ToString();
+                //TempData["Surname"] = data.Surname.ToString();
+                //TempData["Username"] = data.Username.ToString();
+                //TempData["Password"] = data.Password.ToString();
+                //TempData["Phone"] = data.PhoneNumber.ToString();
+                return RedirectToAction("Index", "UserPanel");
+            }
+            else
+            {
+                return View();
+
+            }
         }
     }
 }
--- MVCLMS/Controllers/UserPanelController.cs
+++ MVCLMS/Controllers/UserPanelController.cs
@@ -3,15 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCLMS.Models.Entity;
 
 namespace MVCLMS.Controllers
 {
     public class UserPanelController : Controller
     {
-        // GET: UserPanel
+        DBLibraryEntities database = new DBLibraryEntities();
+        [HttpGet]
+        [Authorize]
         public ActionResult Index()
         {
-            return View();
+            var mail = Session["Mail"].ToString();
+            var values = database.Users.FirstOrDefault(x => x.Mail == mail);
+            return View(values);
         }
+        [HttpPost]
+        public ActionResult Index2(Users users)
+        {
+            var user = Session["Mail"].ToString();
+            var u = database.Users.FirstOrDefault(x => x.Mail == user);
+            u.Name = users.Name;
+            u.Username = users.Username;
+            u.PhoneNumber = users.PhoneNumber;
+            u.Photo = users.Photo;
+            u.Password = users.Password;
+            database.SaveChanges();
+            return View("Index");
+        }
+
+        public ActionResult MyBooks()
+        {
+            var user = Session["Mail"].ToString();
+            var id = database.Users.Where(x => x.Mail == user.ToString()).Select(z => z.Id).FirstOrDefault();
+            var values = database.Operations.Where(x => x.UserId == id).ToList();
+            return View(values);
+        }
+
     }
 }
